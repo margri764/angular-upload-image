068388427aabd0a7c5a57e89b451ff1cb3455b78
PIPIFLOR\pipiflor <margri764@gamil.com>
2021-04-22 10:06:43 -0300

    todo ok

---
+++ angular-upload-image/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { ReactiveFormsModule } from "@angular/forms";
+import { Routes, RouterModule } from '@angular/router';
+
+// import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImagesComponent } from './images/images.component';
+import { ImageComponent } from './images/image/image.component';
+import { ImageListComponent } from './images/image-list/image-list.component';
+import { environment } from "../environments/environment";
+
+
+const routes: Routes = [
+
+  { path: '', redirectTo: 'image/upload', pathMatch: 'full' },
+  { path: 'image', component: ImagesComponent, children: [
+      { path: 'upload', component: ImageComponent },
+      { path: 'list', component: ImageListComponent }
+     ]
+  }
+];
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImagesComponent, 
+    ImageComponent,
+    ImageListComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    // AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-upload-image/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyD3QGXTCzA-Xo8I4jwhKAeJ-wOffgMN3Dw",
+    authDomain: "angular-image-gallery-14267.firebaseapp.com",
+    databaseURL: "https://angular-image-gallery-14267-default-rtdb.firebaseio.com",
+    projectId: "angular-image-gallery-14267",
+    storageBucket: "angular-image-gallery-14267.appspot.com",
+    messagingSenderId: "520651382008",
+    appId: "1:520651382008:web:0ef0bc9d9a10e30aa8af0d"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
